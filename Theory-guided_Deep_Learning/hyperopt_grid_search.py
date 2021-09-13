2f3657d6c463f70e670d6e371ed3acc8ccfadbaa
Jackie Liu <50015297+SHMingLiu@users.noreply.github.com>
2021-09-13 11:50:08 +0100

    Add files via upload

---
+++ Theory-guided_Deep_Learning/hyperopt_grid_search.py
@@ -0,0 +1,117 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May  2 18:14:24 2020
+
+@author: sl7516
+"""
+
+from trainDNN_tensorflow_cv import build_and_train
+#from trainDNN_tensorflow_standard import build_and_train
+from auxiliary import print_json, save_json_result, load_best_hyperspace
+from hyperopt import STATUS_FAIL, hp, tpe, fmin, Trials
+import tensorflow as tf
+
+import json
+import numpy as np
+import traceback
+import os
+
+# hyperopt space
+space = {
+        # learn rate from 1e-8 - 1e-5
+        'learn_rate_mult': [9e-6,8e-6,7e-6,6e-6,5e-6,4e-6,3e-6,2e-6],
+        'reg2_coeff': [0.1,0.08,0.05,0.03,0.01,0.008,0.005,0.003,0.001,0.0008,0.0005]
+        }
+
+def optimise_DNN(hyper_space,log):
+    # optimise the hyper-parameters of DNN
+    try:
+        model, model_name, result, _ = build_and_train(hyper_space,log)
+        
+        # save training results
+        save_json_result(model_name, result)
+        with open('results_hyperspace.txt.json', 'w') as f:
+            json.dump(
+                result['hyper_space'], f,
+                default=None, sort_keys=False, 
+                indent=4, separators=(',',': ')
+                )
+        
+        model.session.close()
+        del model
+        
+        #return result
+    
+    except Exception as err:
+        try:
+            model.session.close()
+        except:
+            pass
+        err_str = str(err)
+        print (err_str,file=log)
+        traceback_str = str(traceback.format_exc())
+        print (traceback_str,file=log)
+        return {
+                'status': STATUS_FAIL,
+                'err': err_str,
+                'traceback': traceback_str
+                }
+    print ('\n\n',file=log)
+    log.flush()
+        
+
+def get_newHyper(hyper_space,last_hyperspace):
+    # run one hyper-parameters optimisation trial and save results
+    if (space.keys()) == 2:
+        last_lr = last_hyperspace['learn_rate_mult']
+        last_lp2 = last_hyperspace['reg2_coeff']
+        id_lr = hyper_space['learn_rate_mult'].index(last_lr) + 1
+        id_lp2 = hyper_space['reg2_coeff'].index(last_lp2)
+        if id_lr >= len(hyper_space['learn_rate_mult']):
+            id_lr = 0
+            id_lp2 += 1
+        new_hyperspace = {'learn_rate_mult': hyper_space['learn_rate_mult'][id_lr],
+                          'reg2_coeff': hyper_space['reg2_coeff'][id_lp2]}
+    else:
+        last_lr = last_hyperspace['learn_rate_mult']
+        id_lr = hyper_space['learn_rate_mult'].index(last_lr) + 1
+        new_hyperspace = {'learn_rate_mult': hyper_space['learn_rate_mult'][id_lr]}
+    
+    return new_hyperspace
+
+def run_a_trial(log):
+    if not os.path.exists('results_hyperspace.txt.json'):
+        if len(space.keys()) == 2:
+            new_hyperspace = {'learn_rate_mult': space['learn_rate_mult'][0],
+                              'reg2_coeff': space['reg2_coeff'][0]}  
+        else:
+            new_hyperspace = {'learn_rate_mult': space['learn_rate_mult'][0]} 
+    else:
+        with open('results_hyperspace.txt.json', 'r') as f:
+            last_hyperspace = json.load(f)
+        new_hyperspace = get_newHyper(space,last_hyperspace)
+        
+    optimise_DNN(new_hyperspace,log)
+    
+    print ('\nOPTIMISATION STEP COMPLETE.\n',file=log)
+    log.flush()
+    
+    
+############################################################## Run the whole optimisation process
+optimise_trials = 1
+    
+log = open('log_file.txt', 'a')
+for i in range(optimise_trials):
+    try:
+        run_a_trial(log)
+    except Exception as err:
+        err_str = str(err)
+        print (err_str,file=log)
+        traceback_str = str(traceback.format_exc())
+        print (traceback_str,file=log)
+        log.flush()
+log.close()
+
+    
+    
+
