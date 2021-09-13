8023a509a3623ba5979509ee2f4dade330e7dc28
Jackie Liu <50015297+SHMingLiu@users.noreply.github.com>
2021-09-13 11:45:01 +0100

    Update create_input.py

--- Theory-guided_Deep_Learning/create_input.py
+++ Theory-guided_Deep_Learning/create_input.py
@@ -32,6 +32,7 @@ def nearest_2nodesIndex(x,x_all):
 
 
 def input_generate(stroke, max_depth, trainOrtest):
+    # convert the geometry to pixel-based image (grey-scale)
     Top_node_num = 801
     
     stroke = str(stroke)
@@ -78,73 +79,3 @@ def input_generate(stroke, max_depth, trainOrtest):
     Y_INPUT = [round(y_inputi/max_depth,6) for y_inputi in y_input]
 
     return Y_INPUT
-
-'''
-plt.figure(figsize=(10,6))
-plt.scatter(x_input,Y_INPUT,0.3,'b')
-plt.scatter(x_load,depth,0.3,'r')
-plt.xlim(0,1)
-
-
-####################
-####################   Preprocessing
-
-y_gray = [round(y*(255)/max(y_input),3) for y in y_input]
-
-y_gray = np.reshape(y_gray,(1,801))
-gray = y_gray[:]
-for i in range(20):
-    gray = np.vstack((gray,y_gray))
-
-plt.figure(figsize=(10,6))
-plt.imshow(gray, cmap='gray')
-plt.show()
-
-plt.figure(figsize=(10,6))
-X = np.random.random((100, 100)) # sample 2D array
-plt.imshow(X, cmap="gray")
-plt.show()
-'''
-'''
-Top_node_num = 801
-max_depth = 48.1949
-stroke = 19
-fr = open('stroke_%(a)s/load_data.txt'%{'a':stroke})  
-x_load=[]
-y_load=[]
-for i, line in enumerate(fr):
-    if i > 0 and i < (Top_node_num+1):
-        lineArr = line.strip().split() 
-        x_load.append(float(lineArr[1]))
-        y_load.append(float(lineArr[2]))
-fr.close()
-x_load.reverse()
-y_load.reverse()
-    
-x_input = np.linspace(0,80,801)
-y_input = np.ones(801)
-    
-ymax = max(y_load)
-    
-depth = [ymax-yi for yi in y_load]
-index_0 = 0
-for i in range(len(x_input)):
-    if x_input[i] > max(x_load):
-        index_0 = i      # the index threshold for parameter interpolation
-        break
-
-for i in range(len(y_input)):
-    if i == 0:
-        y_input[i] = depth[i]
-    elif i > 0 and i < index_0:
-        node1,node2 = nearest_2nodesIndex(x_input[i],x_load)
-        if x_input[i] == x_load[node1]:
-            y_input[i] = depth[node1]
-            continue
-        y_input[i] = IDW_calc_2(x_input[i],x_load[node1],depth[node1],x_load[node2],depth[node2])
-    else:
-        for j in range(i,len(x_input)):
-            y_input[i] = 0.0
-                
-Y_INPUT = [round(y_inputi/max_depth,3) for y_inputi in y_input]
-'''
